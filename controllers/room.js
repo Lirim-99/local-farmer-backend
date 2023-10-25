bf0b7f674e9e2811128a1e1e7a2d578a12c63cd0
Blend Halili <53181578+Lirim-99@users.noreply.github.com>
2023-10-25 14:02:19 +0200

    added user router and google auth to login using google account

--- controllers/room.js
+++ controllers/room.js
@@ -1,6 +1,6 @@
 export const createRoom = async (req, res) => {
-    //testing resource access
-    res
-      .status(201)
-      .json({ success: true, result: { id: 123, title: 'test room' } });
-  };
+  //testing resource access
+  res
+    .status(201)
+    .json({ success: true, result: { id: 123, title: 'test room' } });
+};
